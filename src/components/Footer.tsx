e4c30f8d5c796cec35d3566c7352faefb79ff141
roque-arantes <mrarantes07@gmail.com>
2025-09-29 15:27:14 -0300

    Criando footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,44 @@
+import Email from "../assets/Icones/email.jpeg";
+import Instagram from "../assets/Icones/icone-instagram.jpeg";
+import Linkedin from "../assets/Icones/icone-linkedin.jpeg";
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#222] text-white text-center p-5 -mt-1.5">
+      <div className="max-w-6xl mx-auto">
+        <p className="mb-2 text-sm md:text-base">
+          &copy; 2025 Projeto Chatbot HC — Todos os direitos reservados.
+        </p>
+        <p className="mb-4 text-xs md:text-sm">
+          Desenvolvido por Giovane Amato, Lucas Vieira e Matheus Roque.
+        </p>
+
+        <div className="flex flex-row items-center justify-center gap-6">
+          <a
+            href="#"
+            title="Instagram"
+            className="text-[#44aaff] hover:underline text-sm md:text-base transition-transform duration-300 hover:scale-110 inline w-8 h-8"
+          >
+            <img src={Instagram} alt="Instagram" />
+          </a>
+          <a
+            href="#"
+            title="LinkedIn"
+            className="text-[#44aaff] hover:underline text-sm md:text-base transition-transform duration-300 hover:scale-110 inline w-8 h-8"
+          >
+            <img src={Linkedin} alt="Linkedin" />
+          </a>
+          <a
+            href="mailto:contato@projetoviagem.com"
+            title="E-mail"
+            className="text-[#44aaff] hover:underline text-sm md:text-base transition-transform duration-300 hover:scale-110 inline w-8 h-8"
+          >
+            <img src={Email} alt="email" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
