64ea5c6d557d23a3e900656d9800480f2c2822f9
RuralGiovane <giovaneamato@gmail.com>
2025-09-30 00:20:19 -0300

    criando Scroll automatico pro topo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ScrollTopPage from "./components/ScrollTopPage"; 
 import Header from "./components/Header";
 import MainHome from "./components/MainHome";
 import Menu from "./routes/Menu"
@@ -14,6 +15,7 @@ function App() {
     <>
       <div className="flex flex-col min-h-screen bg-gradient-to-b from-blue-50 to-white">
         <Header />
+        <ScrollTopPage />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<MainHome />} />
